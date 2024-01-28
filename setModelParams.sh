66702d0e31db39c0e65121a8cb4a76365ba3c5f9
Ryan Rasi <29680273+RyanRasi@users.noreply.github.com>
2024-01-28 22:49:38 +0000

    Generate modelfile from template

---
+++ setModelParams.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+input_file_name="Modelfile.template"
+output_file_name="Modelfile"
+old_line="SYSTEM_MESSAGE"
+system_message="You are designed to be an outfit creator. The current available clothes data is stored in a CSV file titled clothes.csv. Please use this information to generate creative and stylish outfits."
+
+# Check if the input file exists
+if [ -e "$input_file_name" ]; then
+    # Replace the specified line and save as a new file
+    sed "s/$old_line/$system_message/" "$input_file_name" > "$output_file_name"
+    echo "Replacement successful. New file saved as $output_file_name"
+else
+    echo "Error: Input file not found."
+fi
